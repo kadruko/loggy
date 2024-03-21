3d79dced45b4a6f7ddcef22b608eb86cf746e785
I518046 <konrad.schewe@sap.com>
2024-03-21 21:12:03 +0100

    switch to data client api (because data can be buffered in case of no connection)

--- app/src/main/java/com/schewe/pc/loggy/presentation/AudioController.kt
+++ app/src/main/java/com/schewe/pc/loggy/presentation/AudioController.kt
@@ -9,13 +9,18 @@ import android.util.Log
 import androidx.annotation.RequiresPermission
 import com.google.android.gms.tasks.Tasks
 import com.google.android.gms.wearable.ChannelClient
+import com.google.android.gms.wearable.DataItem
+import com.google.android.gms.wearable.DataItemBuffer
+import com.google.android.gms.wearable.DataMap
 import com.google.android.gms.wearable.Node
+import com.google.android.gms.wearable.PutDataMapRequest
 import com.google.android.gms.wearable.Wearable
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import java.io.ByteArrayOutputStream
+import java.util.UUID
 import kotlin.coroutines.resume
 import kotlin.coroutines.suspendCoroutine
 
@@ -24,7 +29,6 @@ class AudioController(private val context: Context) {
         const val RECORDING_RATE = 16000
         const val CHANNEL_IN = AudioFormat.CHANNEL_IN_MONO
         const val FORMAT = AudioFormat.ENCODING_PCM_16BIT
-        const val CHANNEL_PATH = "/audioChannel"
     }
 
     @RequiresPermission(Manifest.permission.RECORD_AUDIO)
@@ -33,45 +37,31 @@ class AudioController(private val context: Context) {
         val audioRecord = AudioRecord(MediaRecorder.AudioSource.MIC, RECORDING_RATE, CHANNEL_IN, FORMAT, bufferSize)
         audioRecord.startRecording()
 
-        val channel = withContext(Dispatchers.IO) { openAudioChannel() }
-        if (channel == null) {
-            Log.e("AudioController", "Failed to open audio channel")
-            return
-        }
-
-        val channelClient = Wearable.getChannelClient(context)
-        val outputStream = withContext(Dispatchers.IO) { Tasks.await(channelClient.getOutputStream(channel)) }
-
         try {
-            val sendThreshold = 1024 * 1024 // 1MB
+            // Datenpuffer Größe auf etwa 50 KB einstellen
+            val sendThreshold = 1024 * 50
             val dataBuffer = ByteArrayOutputStream()
 
-            outputStream.use { output ->
-                val data = ByteArray(bufferSize)
-                withContext(Dispatchers.IO) {
-                    while (audioRecord.recordingState == AudioRecord.RECORDSTATE_RECORDING) {
-                        val read = audioRecord.read(data, 0, data.size)
+            val data = ByteArray(bufferSize)
+            while (audioRecord.recordingState == AudioRecord.RECORDSTATE_RECORDING) {
+                val read = audioRecord.read(data, 0, data.size)
 
-                        if (read > 0) {
-                            dataBuffer.write(data, 0, read)
-                        }
-
-                        // Wenn die Puffergröße größer/gleich 1MB ist, verschicken Sie die Daten
-                        if (dataBuffer.size() >= sendThreshold) {
-                            Log.d("AudioController", "Send")
-                            output.write(dataBuffer.toByteArray())
-                            dataBuffer.reset()
-                        }
-                    }
+                if (read > 0) {
+                    dataBuffer.write(data, 0, read)
                 }
 
-                // Senden Sie die restlichen Daten, wenn die Aufnahme gestoppt wird
-                if (dataBuffer.size() > 0) {
-                    withContext(Dispatchers.IO) {
-                        output.write(dataBuffer.toByteArray())
-                    }
+                // Wenn die Puffergröße größer oder gleich 100KB ist, senden Sie die Daten
+                if (dataBuffer.size() >= sendThreshold) {
+                    Log.d("AudioController", "Send")
+                    sendAudioData(dataBuffer.toByteArray())
                     dataBuffer.reset()
                 }
+            }
+
+            // die verbleibenden Daten senden, wenn die Aufnahme gestoppt wird
+            if (dataBuffer.size() > 0) {
+                sendAudioData(dataBuffer.toByteArray())
+                dataBuffer.reset()
             }
         } finally {
             audioRecord.stop()
@@ -79,62 +69,28 @@ class AudioController(private val context: Context) {
         }
     }
 
-    private suspend fun openAudioChannel(): ChannelClient.Channel? {
-        val nodes = suspendCoroutine<Result<MutableList<Node>>> { continuation ->
-            Wearable.getNodeClient(context.applicationContext).connectedNodes
-                .addOnSuccessListener {
-                    continuation.resume(Result.success(it))
-                }
-                .addOnFailureListener {
-                    continuation.resume(Result.failure(it))
-                }
-        }
-
-        val node = nodes.getOrNull()?.firstOrNull()
-
-        return node?.let {
-            val channelClient = Wearable.getChannelClient(context)
-            val openChannelTask = suspendCoroutine<Result<ChannelClient.Channel>> { continuation ->
-                channelClient.openChannel(it.id, CHANNEL_PATH)
-                    .addOnSuccessListener { channel ->
-                        continuation.resume(Result.success(channel))
-                    }
-                    .addOnFailureListener { error ->
-                        continuation.resume(Result.failure(error))
-                    }
-            }
-
-            return openChannelTask.getOrNull()
-        }
-    }
-
     private suspend fun sendAudioData(audioData: ByteArray) {
-        Log.d("X", "Send audio data")
-        val nodes = suspendCoroutine<Result<MutableList<Node>>> { continuation ->
-            Wearable.getNodeClient(context.applicationContext).connectedNodes
-                .addOnSuccessListener {
-                    continuation.resume(Result.success(it))
-                }
-                .addOnFailureListener {
-                    continuation.resume(Result.failure(it))
-                }
+
+        // Erstelle DataMap, das die Audiodaten enthält
+        val dataMap = DataMap().apply {
+            putByteArray("audio_data", audioData)
         }
-        Log.d("X", "Found connected node")
 
-        val node = nodes.getOrNull()?.firstOrNull()
+        // Erstelle eine Anforderung zur Platzierung eines DataItem mit eindeutigem Pfad in der Data Layer
+        val putDataReq = PutDataMapRequest.create("/path/to/audio/data/${UUID.randomUUID()}")
+        putDataReq.dataMap.putAll(dataMap)
 
-        Log.d("X", "node $node")
+        val request = putDataReq.asPutDataRequest().setUrgent()
 
-        node?.let {
-            val channelClient = Wearable.getChannelClient(context)
-            val openChannelTask = channelClient.openChannel(it.id, CHANNEL_PATH)
+        // Sende das DataItem
+        val dataClient = Wearable.getDataClient(context)
 
-            openChannelTask.addOnSuccessListener { channel ->
-                val outputStreamTask = channelClient.getOutputStream(channel)
-                outputStreamTask.addOnSuccessListener { outputStream ->
-                    outputStream.use { it.write(audioData) }
-                }
-            }
+        withContext(Dispatchers.IO) {
+           Tasks.await(dataClient.putDataItem(request))
+
+            // Wir haben in diesem Fall kein enum "isSuccess" oder "isSuccessful" zur Verfügung.
+            // Die erfolgreiche Veröffentlichung des DataItem sollte nicht zu einer Exception führen.
+            Log.d("AudioController", "Data sent successfully")
         }
     }
 }
